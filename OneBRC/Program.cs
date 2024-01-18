974c0bc4545308320f2cb24fbbd559fe9f5c6523
Pedro Travi <pedrotravi@microsoft.com>
2024-01-18 14:00:14 -0300

    Replacing Marvin32 with XxHash3.

--- OneBRC/Program.cs
+++ OneBRC/Program.cs
@@ -5,7 +5,6 @@ using System.IO.MemoryMappedFiles;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Runtime.Intrinsics;
-using System.Runtime.Intrinsics.X86;
 
 namespace OneBRC;
 
--- OneBRC/Utf8StringUnsafeEqualityComparer.cs
+++ OneBRC/Utf8StringUnsafeEqualityComparer.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.IO.Hashing;
 using System.Numerics;
 using System.Runtime.CompilerServices;
 
@@ -6,9 +7,6 @@ namespace OneBRC
 {
     internal readonly struct Utf8StringUnsafeEqualityComparer : IEqualityComparer<Utf8StringUnsafe>
     {
-        private const uint Hash1Start = (5381 << 16) + 5381;
-        private const uint Factor = 1_566_083_941;
-
         public unsafe bool Equals(Utf8StringUnsafe x, Utf8StringUnsafe y)
         {
             return SpanHelpers.SequenceEqual(ref Unsafe.AsRef<byte>(x.Pointer), ref Unsafe.AsRef<byte>(y.Pointer), x.Length);
@@ -16,30 +14,7 @@ namespace OneBRC
 
         public unsafe int GetHashCode([DisallowNull] Utf8StringUnsafe obj)
         {
-            return Marvin.ComputeHash32(ref Unsafe.AsRef<byte>(obj.Pointer), obj.Length, Hash1Start, Factor);
-        }
-
-        public unsafe int GetHashCodeSwar([DisallowNull] Utf8StringUnsafe obj)
-        {
-            int length = (int)obj.Length;
-            uint hash1, hash2;
-            hash1 = Hash1Start;
-            hash2 = (BitOperations.RotateLeft(hash1, 5) + hash1) ^ obj.Length;
-
-            ref var r = ref Unsafe.AsRef<uint>(obj.Pointer);
-            while (length >= 8)
-            {
-                hash1 = (BitOperations.RotateLeft(hash1, 5) + hash1) ^ r;
-                hash2 = (BitOperations.RotateLeft(hash2, 5) + hash2) ^ Unsafe.Add(ref r, 1);
-                r = ref Unsafe.Add(ref r, 2);
-                length -= 8;
-            }
-            ref var b = ref Unsafe.As<uint, byte>(ref r);
-            while (length-- > 0)
-            {
-                hash2 = (BitOperations.RotateLeft(hash2, 5) + hash2) ^ Unsafe.Add(ref b, length + 1);
-            }
-            return (int)(hash1 + (hash2 * Factor));
+            return XxHash3.HashToUInt64(obj.Span).GetHashCode();
         }
     }
 }
