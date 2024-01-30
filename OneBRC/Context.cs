516c9ec8a4acec0d2aeda438363a6fa0b996075a
Pedro Travi <pedrotravi@microsoft.com>
2024-01-30 14:41:01 -0300

    Dumping all the keys on error

--- OneBRC/Context.cs
+++ OneBRC/Context.cs
@@ -18,8 +18,13 @@ namespace OneBRC
 
         internal unsafe Statistics Get(ref readonly Utf8StringUnsafe key)
         {
-            if(!Keys.TryGetValue(key, out var value))
+            if (!Keys.TryGetValue(key, out var value))
+            { 
+                foreach (var k in Keys.Keys) {
+                    Console.WriteLine(k);
+                }
                 throw new KeyNotFoundException($"Key: {key.ToString()} not found");
+            }
             return value;
         }
     }
